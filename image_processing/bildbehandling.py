0196b966692285af5d7e57f5ddc644f043b35072
Ludvig Lifting <ludvig.lifting@outlook.com>
2022-10-12 16:57:37 +0200

    progress on image processing, added uuid in app

--- image_processing/bildbehandling.py
+++ image_processing/bildbehandling.py
@@ -1,3 +1,4 @@
+from email.mime import image
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
@@ -8,37 +9,45 @@ def main():
     file = "/test_color500x500.jpg"
     print(dir_path + file)
 
-    fig = plt.figure()
+    image_BGR = cv2.imread(dir_path + file, cv2.IMREAD_GRAYSCALE)
+    gray = image_BGR #cv2.cvtColor(image_BGR, cv2.COLOR_BGR2GRAY)
 
-    image_BGR = cv2.imread(dir_path + file, cv2.IMREAD_COLOR)
-    gray = cv2.cvtColor(image_BGR, cv2.COLOR_BGR2GRAY)
     #gray = cv2.imread(dir_path + "/test500x500.jpg", cv2.IMREAD_COLOR)
     #sub1 = plt.subplot(4, 2, 1)
     #plt.imshow(gray)
+    resultimage = np.zeros((800, 800))
+    for i in range(1, 6, 2):
+        #sobel_x = cv2.Sobel(gray, cv2.CV_8U, 1, 0, ksize=i, scale=1)
+        #sobel_y = cv2.Sobel(gray, cv2.CV_8U, 0, 1, ksize=i, scale=1)
+        sobel_x = cv2.normalize(cv2.Sobel(gray, cv2.CV_8U, 1, 0, ksize=i, scale=1), resultimage, 0, 1, cv2.NORM_MINMAX)
+        sobel_y = cv2.normalize(cv2.Sobel(gray, cv2.CV_8U, 0, 1, ksize=i, scale=1), resultimage, 0, 1, cv2.NORM_MINMAX)
+        sub2 = plt.subplot(4, 2, i)
+        plt.imshow(sobel_x, cmap="gray")
+        sub3 = plt.subplot(4, 2, i+1)
+        plt.imshow(sobel_y, cmap="gray")
     
-    
-    sobel_x = cv2.Sobel(gray, 2, 1, 0, ksize=3, scale=1)
-    sobel_y = cv2.Sobel(gray, 2, 0, 1, ksize=3, scale=1)
-    sub2 = plt.subplot(2, 2, 1)
-    plt.imshow(sobel_x)
-    sub3 = plt.subplot(2, 2, 2)
-    plt.imshow(sobel_y)
-    #prewitt
-    #robert = cv2.rober
-    laplacian = cv2.Laplacian(gray, 2)
-    sub4 = plt.subplot(2, 2, 3)
-    plt.imshow(laplacian)
 
-    canny = cv2.Canny(gray, 50, 100)
-    sub5 = plt.subplot(2, 2, 4)
-    plt.imshow(canny)
-
-    #sub1.title.set_text('Original')
-    sub2.title.set_text('Sobel x')
-    sub3.title.set_text('Sobel y')
-    sub4.title.set_text('Laplacian')
-    sub5.title.set_text('Canny')
+        #sub1.title.set_text('Original')
+        sub2.title.set_text(f'Sobel x with kernel size {i}')
+        sub3.title.set_text(f'Sobel y with kernel size {i}')
+    #plt.tight_layout()
+    plt.subplots_adjust(
+        top=0.965,
+        bottom=0.0,
+        left=0.13,
+        right=0.9,
+        hspace=0.225,
+        wspace=0.14)
     plt.show()
+
+    """
+    top=0.965,
+    bottom=0.0,
+    left=0.13,
+    right=0.9,
+    hspace=0.225,
+    wspace=0.14
+    """
     
 
 if __name__ == '__main__':
