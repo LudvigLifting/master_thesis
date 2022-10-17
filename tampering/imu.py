8807347a4adaf54b9a791995e9e69fdb1fae353c
Fredrik Berg <fredrik.nils.berg@gmail.com>
2022-10-17 10:57:48 +0200

    Allt som gjorts för IMUn innan det pausats

--- tampering/imu.py
+++ tampering/imu.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
 import time
 import board
 import adafruit_bno055
@@ -8,6 +5,8 @@ import adafruit_bno055
 
 i2c = board.I2C()
 sensor = adafruit_bno055.BNO055_I2C(i2c)
+sensor.mode = adafruit_bno055.MAGGYRO_MODE
+
 
 def temperature():
     result = sensor.temperature
@@ -22,10 +21,21 @@ while True:
     #print("Accelerometer (m/s^2): {}".format(sensor.acceleration))
     #print("Magnetometer (microteslas): {}".format(sensor.magnetic))
     #print("Gyroscope (rad/sec): {}".format(sensor.gyro))
+    print("Calibration {} done".format(sensor.calibration_status))
     print("Euler angle: {}".format(sensor.euler))
     #print("Quaternion: {}".format(sensor.quaternion))
     #print("Linear acceleration (m/s^2): {}".format(sensor.linear_acceleration))
     #print("Gravity (m/s^2): {}".format(sensor.gravity))
     print()
+    time.sleep(3)
 
-    time.sleep(5)
+"""
+Att göra för IMUn: Få systemet kalibrerat genom att ta initalvärdena från en första läsning och sedan kunna kalla på dessa från en extern EEEPROM (antagligen raspberry pi:n).
+
+Sedan ska vi få hela projektet att fungera så vi kan testa olika IMU-setups för att avgöra om någon tamperat med brandsläckaren, kan man t.ex. utgå enbart från gyroskopet så att
+man kan minimera konstruktionen on-chip för senare?
+
+Till sist ska även en enkel magnetlist undersökas och se om det räcker för att undersöka så att brandsläckaren ej har tameprats med.
+
+Bra info finns på sidan: https://www.allaboutcircuits.com/projects/bosch-absolute-orientation-sensor-bno055/
+"""
