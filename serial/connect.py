6e5666da2dc4aa2f56427f1702c4248ee06f429c
Fredrik <fredrik.nils.berg@gmail.com>
2022-11-10 15:36:53 +0100

    updated connection file

--- serial/connect.py
+++ serial/connect.py
@@ -1,10 +1,6 @@
 import serial
 
-ser = serial.Serial()
-ser.baudrate = 9600
-ser.port = '/dev/ttyUSB0'
-ser.timeout = 5
-#ser = serial.Serial('/Device/Silabser0', 9600, timeout=5)
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=5)
 ser.write("AT\r")
 response =  ser.readline()
 ser.close()
