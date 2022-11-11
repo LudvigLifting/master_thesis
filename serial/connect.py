30d536f038d1b3722783b49b6f71dc68450068b7
Fredrik Berg <fredrik.berg@mikrodust.com>
2022-11-11 15:40:20 +0100

    updtaed connect script

--- serial/connect.py
+++ serial/connect.py
@@ -3,13 +3,26 @@ import serial.tools.list_ports as port_list
 
 
 # ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=5)
-full_name_port = list(port_list.comports())
-port = [str(l) for l in full_name_port if str(l) == 'COM4 - Silicon Labs CP210x USB to UART Bridge (COM4)']
-if(not port):
-    exit()
-ser = serial.Serial(port[0][:4], 9600, timeout=5) # For use when running on windows
-ser.write(str.encode("AT\r"))
-response =  ser.read(5)
-print (response.decode('UTF-8'))
-ser.close()
+def ver_windows():
+    full_name_port = list(port_list.comports())
+    port = [str(l) for l in full_name_port if str(l) == 'COM4 - Silicon Labs CP210x USB to UART Bridge (COM4)']
+    if(not port):
+        exit()
+    ser = serial.Serial(port[0][:4], 9600, timeout=5) # For use when running on windows
+    ser.write(str.encode("AT\r"))
+    response =  ser.read(5)
+    print (response.decode('UTF-8'))
+    ser.close()
 
+def ver_WSL():
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=5) # For use when running on windows
+    ser.write(str.encode("AT\r"))
+    response =  ser.read(5)
+    print (response.decode('UTF-8'))
+    ser.close()
+
+def main():
+    ver_windows()
+
+if __name__ == '__main__':
+    main()
