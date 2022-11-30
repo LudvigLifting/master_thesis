a470ab4ab02194e5ae14ce10c4c282e344b1df2e
Ludvig Lifting <ludvig.lifting@outlook.com>
2022-11-30 14:57:31 +0100

    Nya bilder med nytt fäste

--- image_processing/visa_bild.py
+++ image_processing/visa_bild.py
@@ -6,6 +6,5 @@ camera.color_effects = None #(128, 128) #svartvit
 resolutions = [(50*i, 50*i) for i in range(1, 11)]
 print(f"Resolutions: {resolutions}")
 
-
 camera.start_preview(fullscreen=False, window=(100, 20, 1000, 1000))
-sleep(5)
+sleep(20) 
