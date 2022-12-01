5f6033b5c652299e15da343d7aee6411db5fab74
Ludvig Lifting <ludvig.lifting@outlook.com>
2022-12-01 15:31:04 +0100

    many added to c

--- image_processing/C/image_processing.c
+++ image_processing/C/image_processing.c
@@ -17,20 +17,31 @@ typedef struct Size Size;
 void delete_arr(unsigned char** arr, Size size){
 
     for(int i = 0; i < size.rows; i++){
+
         free(arr[i]);
     }
     free(arr);
 }
 
-unsigned char** create_arr(Size size){
-
-    //Ändra till malloc när allt funkar (Förutom den som padding kallar)
+unsigned char** create_arr(Size size, bool mal){
 
     //printf("Creating array with size: %dx%d\n", size.rows, size.cols);
 
-    unsigned char** arr = calloc(size.rows, sizeof(char*));
-    for(int i = 0; i < size.rows; i++){
-        arr[i] = calloc(size.cols, sizeof(char));
+    unsigned char** arr;
+
+    if(mal){
+        arr = malloc(size.rows * sizeof(char*));
+        for(int i = 0; i < size.rows; i++){
+
+            arr[i] = malloc(size.cols * sizeof(char));
+    }
+    }
+    else{
+        arr = calloc(size.rows, sizeof(char*));
+        for(int i = 0; i < size.rows; i++){
+
+            arr[i] = calloc(size.cols, sizeof(char));
+        }
     }
 
     return arr;
@@ -62,7 +73,9 @@ void print_arr(unsigned char** arr, Size arrsize){
 
     printf("[");
     for(int i = 0; i < arrsize.rows; i++){
+
         printf("[");
+
         for(int j = 0; j < arrsize.cols; j++){
             
             if( j < arrsize.cols - 1 ){
@@ -84,8 +97,7 @@ unsigned char** load_file(Size imsize, char fileName[]){
     char cwd[PATH_MAX];
     getcwd(cwd, sizeof(cwd));
 
-    printf("Loading file \"%s\"...\n", strcat(cwd, fileName));
-    printf("cwd = %s\n", cwd);
+    printf("Loading file \"%s\"\n...\n", strcat(cwd, fileName));
     
     FILE *fptr = fopen(cwd, "r");
 
@@ -94,7 +106,7 @@ unsigned char** load_file(Size imsize, char fileName[]){
         exit(-1);
     }
 
-    unsigned char** image_matrix = create_arr(imsize);
+    unsigned char** image_matrix = create_arr(imsize, true);
 
     int i = 0;
     int j = 0;
@@ -129,9 +141,10 @@ unsigned char** pad(unsigned char** image, Size *imsize){
     //Update imsize and allocate new expanded array
     imsize->rows += 2;
     imsize->cols += 2;
-    unsigned char** expanded = create_arr(*imsize);
+    unsigned char** expanded = create_arr(*imsize, false); //calloc här, kanske är snabbare att köra malloc och sen manuellt lägga till nollor
 
     for(int i = 0; i < imsize->rows - 2; i++){
+
         memcpy(&expanded[i + 1][1], image[i], (size_t)(imsize->cols*sizeof(char) - 2));
     }
 
@@ -145,9 +158,10 @@ unsigned char** unpad(unsigned char** image, Size *imsize){
 
     imsize->rows -= 2;
     imsize->cols -= 2;
-    unsigned char** reduced = create_arr(*imsize);
+    unsigned char** reduced = create_arr(*imsize, false);
 
     for(int i = 0; i < imsize->rows; i++){
+
         memcpy(reduced[i], &image[i + 1][1], imsize->cols*sizeof(char));
     }
 
@@ -160,9 +174,10 @@ unsigned char** unpad(unsigned char** image, Size *imsize){
 //Kanske inte behövs alls, blir typ 3ggr så långsamt med en algoritm + subarray
 unsigned char** subarray(Size ker_size, unsigned char** image, int row, int col){
 
-    unsigned char** sub = create_arr(ker_size);
+    unsigned char** sub = create_arr(ker_size, true);
     
     for(int i = 0; i < ker_size.rows; i++){
+
         memcpy(sub[i], &image[row + i - 1][col - 1], ker_size.cols*sizeof(char));
     }
 
@@ -171,14 +186,13 @@ unsigned char** subarray(Size ker_size, unsigned char** image, int row, int col)
 
 unsigned char** sobel(unsigned char** image, Size imsize, bool xy){
 
-    unsigned char** sobeld = create_arr(imsize);
+    unsigned char** sobeld = create_arr(imsize, true);
     int x = 0;
     int y = 0;
 
-    for(int i = 1; i < imsize.rows-1; i++)
-    {
-        for(int j = 1; j < imsize.cols-1; j++)
-        {
+    for(int i = 1; i < imsize.rows-1; i++){
+        for(int j = 1; j < imsize.cols-1; j++){
+
             x = 0.25*(-(image[i - 1][j - 1] + image[i + 1][j - 1]) + image[i - 1][j + 1] + image[i + 1][j + 1] + 2*(image[i][j + 1] - image[i][j - 1]));
             if(xy){
                 y = 0.25*(-(image[i + 1][j - 1] + image[i + 1][j + 1]) + image[i - 1][j - 1] + image[i - 1][j + 1] + 2*(image[i - 1][j] - image[i + 1][j]));
@@ -197,13 +211,12 @@ unsigned char** sobel(unsigned char** image, Size imsize, bool xy){
 
 unsigned char** threshold(unsigned char** image, Size imsize, int offset){
 
-    unsigned char** thresholded = create_arr(imsize);
+    unsigned char** thresholded = create_arr(imsize, true);
     int mean;
 
     for(int i = 1; i < imsize.rows - 1; i++){
         for(int j = 1; j < imsize.cols - 1; j++){
 
-            //0.1 pga ungefär /9
             mean = 0.1*(image[i-1][j-1] + image[i-1][j] + image[i-1][j+1] + image[i][j-1] + image[i][j] + image[i][j+1] + image[i+1][j-1] + image[i+1][j] + image[i+1][j+1]); 
 
             thresholded[i][j] = (unsigned char) ((image[i][j] < ( mean - offset )) ? 255 : 0);
@@ -218,9 +231,10 @@ unsigned char** threshold(unsigned char** image, Size imsize, int offset){
 
 unsigned char** diff(unsigned char** ref, unsigned char** test, Size imsize){
 
-    unsigned char** difference = create_arr(imsize);
+    unsigned char** difference = create_arr(imsize, true);
     for(int i = 0; i < imsize.rows; i++){
         for(int j = 0; j < imsize.cols; j++){
+
             difference[i][j] = (unsigned char) abs(ref[i][j] - test[i][j]);
         }
     }
@@ -230,8 +244,79 @@ unsigned char** diff(unsigned char** ref, unsigned char** test, Size imsize){
     return difference;
 }
 
+//Om vi ska ha detta
+int calc_floor(unsigned char** diff, Size size){
+    
+    int mean = 0;
+    for(int i = 0; i < size.rows; i++){
+        for(int j = 0; j < size.cols; j++){
+
+            mean += diff[i][j];
+        }
+    }
+
+    return (int)(mean/(size.rows * size.cols));
+}
+
+bool decision(unsigned char** diff, Size size, int floor){
+
+    int nbr = 0;
+    bool decide = false;
+
+    //Count white pixels
+    for(int i = 0; i < size.rows; i++){
+        for(int j = 0; j < size.cols; j++){
+
+            if(diff[i][j] > 0){
+                nbr++;
+            }
+        }
+    }
+
+    if(nbr > floor){
+        decide = true;
+    }
+
+    return decide;
+}
+
+void process(char file[], char output[]){
+
+    Size imsize = { .rows = 200, .cols = 200 };
+    unsigned char** image = create_arr(imsize, true);
+
+    image = load_file(imsize, file);
+    image = pad(image, &imsize);
+    image = sobel(image, imsize, false);
+    image = threshold(image, imsize, 5);
+    image = unpad(image, &imsize);
+
+    export_csv(image, imsize, output);
+}
+
+void many_images(){
+
+    char file[13] = "/many/";
+    char buff[100];
+    char out[12] = "/out/";
+
+    for(int i = 0; i < 100; i++){
+        sprintf(&file[6], "%d", i);
+        sprintf(&out[5], "%d", i);
+        strcat(file, ".csv\0");
+        strcat(out, ".csv\0");
+
+        process(file, out);
+
+        file[6] = '\0';
+        out[5] = '\0';
+    }
+}
 
 int main(int argc, char **argv){
+
+    many_images();
+    return 0;
 
     clock_t start;
     double elapsed_time;
@@ -262,7 +347,6 @@ int main(int argc, char **argv){
         printf("Execution time: %.3fus\n", 1000000*elapsed_time);
     }
     
-    //"delete_arr" funkar inte ibland, tror vi råkar allokera på stacken istället för heapen, men fattar inte riktigt (man ska/kan inte free:a på stacken)
     if(image != NULL){
         delete_arr(image, imsize);
     }
