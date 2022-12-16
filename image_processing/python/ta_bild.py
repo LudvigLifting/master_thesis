4ed0fe6288ed158782ae269d2ad1503b0f645d7a
Ludvig Lifting <ludvig.lifting@outlook.com>
2022-12-16 16:13:37 +0100

    kldsajkmdsla

--- image_processing/python/ta_bild.py
+++ image_processing/python/ta_bild.py
@@ -3,7 +3,7 @@ from picamera import PiCamera
 
 camera = PiCamera()
 
-camera.resolution = (500, 500)
+camera.resolution = (200, 200)
 camera.start_preview()
 sleep(2)
-camera.capture("test.jpg")
+camera.capture("test2.jpg")
