5e6beb7cbde9ba3e829b61856992a2796fd73518
Ludvig Lifting <ludvig.lifting@outlook.com>
2023-01-19 13:58:49 +0100

    nya bilder

--- image_processing/python/sekvens_ta_bild.py
+++ image_processing/python/sekvens_ta_bild.py
@@ -4,12 +4,15 @@ from picamera import PiCamera
 camera = PiCamera()
 camera.color_effects = (128, 128) #svartvit
 resolution = (200, 200)
-dir_path = "/home/exjobb/code/master_thesis/image_processing/pictures/many/" 
+dir_path = "/home/exjobb/code/master_thesis/image_processing/" 
 
-nbr_images = 100
-for i in range(nbr_images):
+nbr_images = 200
+for i in range(101, 202):
     print(f"image {i}")
     camera.resolution = resolution
-    sleep(0.2)
-    camera.capture(dir_path + str(i) + ".jpg")
+    sleep(2)
+    if i == 101:
+        camera.capture(dir_path + "ref2" + ".jpg")
+    else: 
+        camera.capture(dir_path + str(i) + ".jpg")
     print("done")
